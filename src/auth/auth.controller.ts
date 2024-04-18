9182031aefa2cd555d407eefa17feb1fb805c2e8
Mikhail Smirnov <urine89@mail.ru>
2024-04-18 17:09:06 +0300

    Delete unused controller method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,8 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { UserDto } from './dto/user-dto';
 import { User } from '../users/user.model';
-import { JwtAuthGuard } from './jwt-auth.guard';
-import { AdminGuard } from './admin.guard';
 
 @ApiTags('Authorization')
 @Controller('auth')
@@ -16,15 +14,5 @@ export class AuthController {
   @Post('/login')
   login(@Body() userDto: UserDto) {
     return this.authService.login(userDto);
-  }
-
-  @ApiOperation({ summary: 'Get all users' })
-  @ApiResponse({ status: 200, type: [User] })
-  @Get('/:token')
-  @UseGuards(JwtAuthGuard)
-  @UseGuards(AdminGuard)
-  getUserInfo(@Param('token') token: string) {
-    console.log(token);
-    // return this.authService.getUserInfo();
   }
 }
